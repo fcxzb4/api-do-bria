46823ea294eeb6e8e2140490075fb127b230de72
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-11-18 21:20:36 -0300

    sadasd

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 
 export class LoginDto {
-    IsEmail().
-    @isNotEmpty()
+    @IsEmail()
+    @IsNotEmpty()
     email: string ;
 
-    IsString()
+    @IsString()
     @IsNotEmpty()
-    password:string; 
+    password: string;
 }
